3a969e0d40eb82ff36c193974142d3adac4d94c6
jh2mhs8 <jayrhackett@gmail.com>
2013-01-01 19:29:56 -0700

    Commit

---
+++ RockWeb/Plugins/JayHackett/PageList1.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="PageList1.ascx.cs" Inherits="Plugins_JayHackett_PageListEF" %>
+<asp:TreeView ID="tvDataSet" runat="server" MaxDataBindDepth="2">
+    <Nodes>
+        <asp:TreeNode PopulateOnDemand="True" Text="Pages" Value="Pages"></asp:TreeNode>
+        
+    </Nodes>
+</asp:TreeView>
+
+
+
---
+++ RockWeb/Plugins/JayHackett/PageList1.ascx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+public partial class Plugins_JayHackett_PageListEF : Rock.Web.UI.RockBlock
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindTreeViewControl();
+        }
+    }
+
+    private void BindTreeViewControl()
+    {
+
+        DataSet ds = GetDataSet("Select Id, Name, ParentPageId from Page");
+        DataRow[] Rows = ds.Tables[0].Select("ParentPageId Is Null");
+        for (int i = 0; i < Rows.Length; i++)
+        {
+            TreeNode root = new TreeNode(Rows[i]["Name"].ToString(), Rows[i]["Id"].ToString());
+            root.SelectAction = TreeNodeSelectAction.Expand;
+            CreateNode(root, ds.Tables[0]);
+            tvDataSet.Nodes.Add(root);
+        }
+
+
+    }
+
+    public void CreateNode(TreeNode node, DataTable Dt)
+    {
+        DataRow[] Rows = Dt.Select("ParentPageId =" + node.Value);
+
+        if (Rows.Length == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < Rows.Length; i++)
+        {
+            TreeNode ChildNode = new TreeNode(Rows[i]["Name"].ToString(), Rows[i]["Id"].ToString());
+            node.NavigateUrl = "../RockWeb/page/" + Rows[i]["ParentPageId"].ToString();
+            ChildNode.NavigateUrl = "../RockWeb/page/" + Rows[i]["Id"].ToString();
+            node.ChildNodes.Add(ChildNode);
+            CreateNode(ChildNode, Dt);
+        }
+
+    }
+
+    private DataSet GetDataSet(string Query)
+    {
+        DataSet Ds = new DataSet();
+        string strCon = ConfigurationManager.ConnectionStrings["RockContext"].ConnectionString;
+        SqlConnection Con = new SqlConnection(strCon);
+        SqlDataAdapter Da = new SqlDataAdapter(Query, Con);
+        Da.Fill(Ds);
+        return Ds;
+    }
+}
